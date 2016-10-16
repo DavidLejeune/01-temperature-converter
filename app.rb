8217e437713c498c8488f7b45f3c008eabd8d7f6
David Lejeune <Lejeune.David@outlook.com>
2016-10-16 19:50:08 +0200

    Reset project before another attempt at single responsibility

--- app.rb
+++ app.rb
@@ -4,7 +4,23 @@ require './lib/temperature_read.rb'
 require './lib/temperature_convert.rb'
 #temp_converter = TemperatureConverter.new
 
-puts 'output : read from commandline'
-puts ''
-TemperatureRead.commandline_temperature ARGV[0]
+
+cmdl_input = ARGV[0]
+file_path = "data.txt"
+url_path = "http://labict.be/software-engineering/temperature/api/temperature/fake"
+
+puts "output : read from commandline\n"
+TemperatureConverter.commandline_temperature cmdl_input
+TemperatureConverter.show_output
+puts '------------------------------------------------------------------'
+
+
+puts "output : read from file"
+TemperatureConverter.file_temperature file_path
+TemperatureConverter.show_output
+puts '------------------------------------------------------------------'
+
+puts "output : read from url\n"
+TemperatureConverter.url_temperature url_path
+TemperatureConverter.show_output
 puts '------------------------------------------------------------------'
