8d6278ad35a43f0e7019bbefeb8e33b1274f310b
David Lejeune <Lejeune.David@outlook.com>
2016-10-17 17:52:14 +0200

    Added some constants

--- lib/temperature_convert.rb
+++ lib/temperature_convert.rb
@@ -1,19 +1,33 @@
 #!/usr/bin/env ruby
 
-
-
 class TemperatureConvert
 
 	attr_reader :temp
 	KELVIN = 273.15
+	FAHRENHEIT_OFFSET = 32
+	FAHRENHEIT_SLOPE = Rational(9,5)
 
 #---------------------------------------------------------------------
 #converting
 	  def self.convert(temp)
 	    celcius = temp
 	    fahrenheit = (temp.to_f * (9 / 5) ) + 32
+			#fahrenheit = to_fahrenheit(temp)
 	    kelvin = temp.to_f +  KELVIN
+			#kelvin = to_fahrenheit(temp)
 	    return celcius, fahrenheit, kelvin
 	  end
+
+		# def self.to_celcius(temp)
+		# 	celcius = temp
+		# end
+		#
+		# def self.to_fahrenheit(temp)
+		# 	(to_celcius * FAHRENHEIT_SLOPE) + FAHRENHEIT_OFFSET
+		# end
+		#
+		# def self.to_kelvin(temp)
+		# 	to_celcius + KELVIN
+		# end
 
 end
