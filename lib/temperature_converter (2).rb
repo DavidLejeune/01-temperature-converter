857c9ed31ba0a555ce4a4101a64fe1dd3ed10ff6
David Lejeune <Lejeune.David@outlook.com>
2016-10-24 22:18:53 +0200

    Single Resp + Static

--- lib/temperature_converter (2).rb
+++ lib/temperature_converter (2).rb
@@ -1,8 +1,10 @@
 
 #!/usr/bin/env ruby
 require './lib/temperature_read_commandline.rb'
+require './lib/temperature_read_file.rb'
+require './lib/temperature_read_url.rb'
 
-class TemperatureConverterCommandline
+class TemperatureConverter
 
 
 	attr_reader :temp
@@ -12,9 +14,17 @@ class TemperatureConverterCommandline
 	end
 
 #---------------------------------------------------------------------
-#start converting by reading
-
+#reading
 		def self.commandline_temperature temp
 			TemperatureReadCommandline.commandline_temperature temp
 		end
+
+		def self.file_temperature path
+			TemperatureReadFile.file_temperature path
+		end
+
+		def self.url_temperature url
+			TemperatureReadUrl.url_temperature url
+		end
+
 end
--- lib/temperature_read.rb
+++ lib/temperature_read.rb
@@ -5,7 +5,7 @@ require 'uri'
 require './lib/temperature_convert.rb'
 require './lib/temperature_output.rb'
 
-class TemperatureReadCommandline
+class TemperatureRead
 
 
 	attr_reader :temp
@@ -20,6 +20,18 @@ class TemperatureReadCommandline
 			TemperatureOutput.show_output(TemperatureConvert.convert(temp))
 		end
 
+		def self.file_temperature path
+			@path = path
+			f = File.open(path, "r")
+			TemperatureOutput.show_output(TemperatureConvert.convert(f.sysread(6).to_f))
+		end
 
+		def self.url_temperature url
+			TemperatureOutput.show_output(TemperatureConvert.convert(open_url(url).to_f))
+		end
+
+		def self.open_url(url)
+			Net::HTTP.get(URI.parse(url))
+		end
 
 end
