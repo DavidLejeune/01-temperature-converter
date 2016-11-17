405333b1d75ed8d56333803f8798daee6113eb21
David Lejeune <Lejeune.David@outlook.com>
2016-11-17 19:35:05 +0100

    works for all output option (-t -j -w -a)

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'optparse'
 cmdl_input = ARGV[0]
 file_path = "data.txt"
 url_path = "http://labict.be/software-engineering/temperature/api/temperature/fake"
-
+@show_all=true
 
 
 OptionParser.new do |opts|
@@ -17,25 +17,12 @@ OptionParser.new do |opts|
     puts v.inspect
   end
 
-  opts.on("-t", "--text", Float , "Show text only") do |t|
-      $show_output_style = $show_output_style + 1
-  end
-  opts.on("-w", "--web", Float , "Show html only") do |t|
-      $show_output_style = $show_output_style + 2
-  end
-  opts.on("-j", "--json", Float , "Show json only") do |t|
-      $show_output_style = $show_output_style + 4
-  end
 
   opts.on("-c MYCOMMAND", "--command MYCOMMAND", Float , "Commandline temperature") do |mycommand|
     puts "output : read from commandline".white
     puts "==============================\n\n".white
-    conversion = TemperatureConverterCommandline.commandline_temperature mycommand
-    TemperatureOutput.show_output(conversion)
-    puts '------------------------------------------------------------------'.yellow
+    @conversion = TemperatureConverterCommandline.commandline_temperature mycommand
   end
-
-
 
 
   opts.on("-f [MYFILE]", "--file [MYFILE]", String , "File path") do |myfile|
@@ -44,9 +31,7 @@ OptionParser.new do |opts|
     end
     puts "output : read from file".white
     puts "=======================\n\n".white
-    conversion = TemperatureConverterFile.file_temperature myfile
-    TemperatureOutput.show_output(conversion)
-    puts '------------------------------------------------------------------'.yellow
+    @conversion = TemperatureConverterFile.file_temperature myfile
   end
 
 
@@ -56,9 +41,7 @@ OptionParser.new do |opts|
     end
     puts "output : read from url".white
     puts "======================\n\n".white
-    conversion = TemperatureConverterUrl.url_temperature myurl
-    TemperatureOutput.show_output(conversion)
-    puts '------------------------------------------------------------------'.yellow
+    @conversion = TemperatureConverterUrl.url_temperature myurl
   end
 
 
@@ -67,11 +50,31 @@ OptionParser.new do |opts|
     #puts "output : read from ttl".white
     #puts "======================\n\n".white
     TemperatureConverter.ttl_temperature origin_rb
-    puts '------------------------------------------------------------------'.yellow
   end
 
 
+  opts.on("-t", "--text", Float , "Show text only") do |t|
+    @show_all=false
+    TemperatureOutput.show_to_text(@conversion)
+    puts '------------------------------------------------------------------'.yellow
+  end
+  opts.on("-w", "--web", Float , "Show html only") do |t|
+    @show_all=false
+    TemperatureOutput.show_to_html(@conversion)
+    puts '------------------------------------------------------------------'.yellow
+  end
+  opts.on("-j", "--json", Float , "Show json only") do |t|
+    @show_all=false
+    TemperatureOutput.show_to_json(@conversion)
+    puts '------------------------------------------------------------------'.yellow
+  end
 
+  #if(@show_all)
+  opts.on("-a", "--all", Float , "Show all output formats") do |t|
+    TemperatureOutput.show_output(@conversion)
+    puts '------------------------------------------------------------------'.yellow
+  end
+  #end
 
 
 
