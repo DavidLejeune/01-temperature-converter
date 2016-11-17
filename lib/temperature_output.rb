f4623292b90951cf3b27a7552b7b7186cef8d867
David Lejeune <Lejeune.David@outlook.com>
2016-11-17 19:26:22 +0100

    split up the output class (to use the -t -j- -w or -a   TO BE DONE)

--- lib/temperature_output.rb
+++ lib/temperature_output.rb
@@ -13,23 +13,26 @@ class TemperatureOutput
 #outputting
 
 		def self.show_output(converted_temp)
+			show_to_text(converted_temp)
+			show_to_html(converted_temp)
+			show_to_json(converted_temp)
+		end
 
-
+		def self.show_to_text(converted_temp)
 				puts '>to_text'
 				puts TemperatureOutputToText.to_text(converted_temp[0], converted_temp[1], converted_temp[2])
 				puts ''
-
-				puts '>to_html'
-				puts TemperatureOutputToHtml.to_html(converted_temp[0], converted_temp[1], converted_temp[2])
-				puts ''
-
-
-				puts '>to_json'
-				puts TemperatureOutputToJson.to_json(converted_temp[0], converted_temp[1], converted_temp[2])
-				puts ''
-
-
 		end
 
+		def self.show_to_html(converted_temp)
+			puts '>to_html'
+			puts TemperatureOutputToHtml.to_html(converted_temp[0], converted_temp[1], converted_temp[2])
+			puts ''
+		end
 
+		def self.show_to_json(converted_temp)
+			puts '>to_json'
+			puts TemperatureOutputToJson.to_json(converted_temp[0], converted_temp[1], converted_temp[2])
+			puts ''
+		end
 end
